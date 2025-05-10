00eaaa6da40480a1a441d0de248fc3b553dd97b7
Ahmed Abdelsalam <ahmedakka97@gmail.com>
2025-05-10 18:06:28 +0300

    Final working backend with API route /api/products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,15 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
 
 class ProductController extends Controller
 {
+    /**
+     * Display a listing of the products.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function index()
     {
-        return response()->json([
+        $products = [
             ['id' => 1, 'name' => 'Apples', 'price' => 2.5],
-            ['id' => 2, 'name' => 'Bananas', 'price' => 3.0],
-            ['id' => 3, 'name' => 'Milk', 'price' => 4.5],
-        ]);
+            ['id' => 2, 'name' => 'Bread', 'price' => 1.75],
+            ['id' => 3, 'name' => 'Milk', 'price' => 3.0]
+        ];
+
+        return response()->json($products);
     }
 }
