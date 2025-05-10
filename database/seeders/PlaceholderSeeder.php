8aedd21b8f3be9798112bafb550705d6f3333519
Ahmed Abdelsalam <ahmedakka97@gmail.com>
2025-05-10 15:22:53 +0300

    Add missing seeders folder for Heroku

---
+++ database/seeders/PlaceholderSeeder.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class PlaceholderSeeder extends Seeder
+{
+    public function run(): void
+    {
+        // This is a placeholder to prevent build failure
+    }
+}
